74f661f9de0e694334dec3fa04ff9370c8bc21a0
SomeOverflow <master@dounutmail.de>
2021-11-03 19:46:38 +0100

    Add Paper support

---
+++ src/main/java/me/someoverflow/teamchat/spigot/SListener.java
@@ -0,0 +1,57 @@
+package me.someoverflow.teamchat.spigot;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+
+/**
+ * @author SomeOverflow
+ */
+public class SListener implements Listener {
+
+    @EventHandler
+    public void handleChat(AsyncPlayerChatEvent event) {
+        if (event.getMessage().startsWith("#reloadtc")) {
+            if (event.getPlayer().hasPermission(STeamChat.getInstance().getReloadPermission())) {
+                event.getPlayer().sendMessage("[TeamChat] Reload config");
+                STeamChat.getInstance().reloadConfig();
+                event.getPlayer().sendMessage("[TeamChat] Config reloaded");
+                event.setCancelled(true);
+            }
+            return;
+        }
+        for (String type :
+                STeamChat.getInstance().getChatTypes()) {
+            String[] strings = type.split(";");
+            String permission = strings[1];
+            if (event.getMessage().startsWith(strings[2])) {
+                if (permission.equals("?")) permission = null;
+                if (permission == null || event.getPlayer().hasPermission(permission))
+                    handle(event, event.getPlayer(), permission, strings[2], strings[3], strings[0]);
+                break;
+            }
+        }
+    }
+
+    private void handle(AsyncPlayerChatEvent event, Player player, String permission, String command, String prefix, String name) {
+        String message = event.getMessage().replaceFirst(command + " ", "");
+        for (Player players : STeamChat.getInstance().getServer().getOnlinePlayers()) {
+            if (permission == null || players.hasPermission(permission)) {
+                if (players.equals(player)) {
+                    player.sendMessage(STeamChat.getInstance().getMessageSent()
+                            .replaceAll("%prefix%", prefix)
+                            .replaceAll("%sender%", player.getDisplayName())
+                            .replaceAll("%message%", message)
+                            .replaceAll("%server%", player.getServer().getName()));
+                } else player.sendMessage(STeamChat.getInstance().getMessageReceived()
+                        .replaceAll("%prefix%", prefix)
+                        .replaceAll("%sender%", player.getDisplayName())
+                        .replaceAll("%message%", message)
+                        .replaceAll("%server%", player.getServer().getName()));
+            }
+        }
+        event.setCancelled(true);
+    }
+
+}
---
+++ src/main/java/me/someoverflow/teamchat/spigot/STeamChat.java
@@ -0,0 +1,95 @@
+package me.someoverflow.teamchat.spigot;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.List;
+import java.util.logging.Level;
+
+/**
+ * @author SomeOverflow
+ */
+public final class STeamChat extends JavaPlugin {
+
+    private static STeamChat instance;
+    private ConfigAccessorPaper configAccessor;
+
+    private String reloadPermission;
+    private String messageSent;
+    private String messageReceived;
+    private List<String> chatTypes;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+        instance.getLogger().log(Level.INFO, "Loading Config...");
+        configAccessor = new ConfigAccessorPaper(this, "config.yml");
+        configAccessor.saveDefaultConfig();
+        configAccessor.getConfig().options().copyDefaults(true);
+        configAccessor.saveConfig();
+        instance.getLogger().log(Level.INFO, "Config Loaded");
+
+        instance.getLogger().log(Level.INFO, "Loading messages from Config...");
+        messageSent = configAccessor.getConfig().getString("message.sent", "%prefix% \u00A78> \u00A7fYou \u00A78>> \u00A7f%message%");
+        messageReceived = configAccessor.getConfig().getString("message.received", "%prefix% \u00A78> \u00A7f%sender% \u00A78>> \u00A7f%message%");
+        reloadPermission = configAccessor.getConfig().getString("permission.reload", "tc.reload");
+        instance.getLogger().log(Level.INFO, "Messages from Config Loaded");
+
+        instance.getLogger().log(Level.INFO, "Chat types from Config...");
+        chatTypes = configAccessor.getConfig().getStringList("chatTypes");
+        if (chatTypes.isEmpty()) {
+            instance.getLogger().log(Level.INFO, "There are no chat types in the config...");
+        } else {
+            instance.getLogger().log(Level.INFO, "Loaded types:");
+            for (String s : chatTypes) {
+                String[] strings = s.split(";");
+                instance.getLogger().log(Level.INFO, strings[0] + " | " + strings[1] + " | " + strings[2] + " | " + strings[3]);
+            }
+        }
+        instance.getLogger().log(Level.INFO, "Chat types from Config Loaded");
+
+        instance.getLogger().log(Level.INFO, "Loading Listener...");
+        Bukkit.getPluginManager().registerEvents(new SListener(), this);
+        instance.getLogger().log(Level.INFO, "Listener Loaded");
+        instance.getLogger().log(Level.INFO, "Plugin started");
+    }
+
+    @Override
+    public void onDisable() {
+        instance.getLogger().log(Level.INFO, "Plugin disabled");
+        instance.getLogger().log(Level.INFO, "Bye <3");
+    }
+
+    public void reloadConfig() {
+        configAccessor.reloadConfig();
+        messageSent = configAccessor.getConfig().getString("message.sent", "%prefix% \u00A78> \u00A7fYou \u00A78>> \u00A7f%message%");
+        messageReceived = configAccessor.getConfig().getString("message.received", "%prefix% \u00A78> \u00A7f%sender% \u00A78>> \u00A7f%message%");
+        chatTypes = configAccessor.getConfig().getStringList("chatTypes");
+        reloadPermission = configAccessor.getConfig().getString("permission.reload", "tc.reload");
+        if (chatTypes.isEmpty()) {
+            instance.getLogger().log(Level.INFO, "There are no chat types in the config...");
+        } else {
+            this.getLogger().log(Level.INFO, "Reload config with types:");
+            for (String s : chatTypes) {
+                String[] strings = s.split(";");
+                instance.getLogger().log(Level.INFO, strings[0] + " | " + strings[1] + " | " + strings[2] + " | " + strings[3]);
+            }
+        }
+    }
+
+    public static STeamChat getInstance() {
+        return instance;
+    }
+    public String getMessageSent() {
+        return messageSent;
+    }
+    public String getMessageReceived() {
+        return messageReceived;
+    }
+    public List<String> getChatTypes() {
+        return chatTypes;
+    }
+    public String getReloadPermission() {
+        return reloadPermission;
+    }
+}
